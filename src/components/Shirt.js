9fd7464822248082ad0a3528e2f9297c3573e354
logroa <logroa@umich.edu>
2022-03-07 00:30:28 -0500

    rebuilt webpack from scratch

--- src/components/Shirt.js
+++ src/components/Shirt.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Shirt.css';
-require('dotenv').config()
+//require('dotenv').config()
 
 class Shirt extends React.Component {
     constructor(props) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path')
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
  template: "./src/index.html",
@@ -5,19 +6,31 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
-mode: 'development',
-  module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: "babel-loader"
-   }
- },
-  {
-   test: /\.css$/,
-   use: ["style-loader", "css-loader"]
-  }
-]},
- plugins: [htmlPlugin]
-};
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: { // new concept, loaders
+        rules: [
+          {
+            test: /\.js$/,
+            //include: path.resolve('./src/index.js'), // file to transpile
+            exclude: /node_modules/,
+            loader: "babel-loader", // loaders referenced
+            //exclude: /node_modules/, // we do not need to transpile other libraries
+            //query: require('./.babelrc'), // reference to babel rules
+          },
+          {
+            test: /\.html$/,
+            loader: "html-loader"
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          }
+        ]
+    },
+    plugins: [
+        htmlPlugin
+    ]
+}

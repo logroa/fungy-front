d8e926170480262c97d80083c1e9411601626b1c
logroa <logroa@umich.edu>
2022-03-29 00:03:19 -0400

    our credentials links

--- src/App.js
+++ src/App.js
@@ -35,15 +35,32 @@ class Intro extends React.Component {
     return (
       <div id="intro">
         <div id="intro1">
-          weird funny spiel
+          <p>Hey, my name is <b>Richard</b>.  And I'm the Chief Culture Officer at FungeMyTees.  We specialize in 
+           both making money and looking really cool while doing it.  I bet you're wondering how I found myself in
+           such a distinguished position at such a distinguished company.  Although I could tell you story after story of
+           astute day trades, riverboat gambling conquests, and high profile government payoffs, it all boiled down, at least 
+           for me, to knowing the definition of the word “fungible”.  Yep, that's it, baby.</p>
+
+          <p>Our goal here is to invest your money in various tokens, coins and other safe, high-yield locations.  Please 
+            take a look at our extensive catalogue of inspiringly political, thought-provoking apparel.  All profits will 
+            be put in your name towards a mutual fund of sorts aimed at, of course, maximizing return according to our very 
+            loose definition of “value”.  Sizable returns will be paid back to investors, and on the off chance we do squander 
+            your money, you'll walk away with a shirt wearable in very few social settings!  You're welcome!  Again, my name is <b>Richard</b>.  Look 
+            me up!</p>
+
+          <p>Click below to read more about how we got here and what we're hoping to do!</p>
           <br/>
-          <button className="gatelock" onClick={() => this.introSwitch("1", "2")} >Continue</button>
-          <br/>
-          <button className="gatelock" onClick={() => this.openGate()} >Buy shirt</button>
+          <button className="gatelock" onClick={() => this.introSwitch("1", "2")} >Read more</button>
+          <br/> <br/>
+          <button className="gatelock" onClick={() => this.openGate()} >No thanks</button>
+          <br /> <br /> <br />
+          <a href="https://www.chuckecheese.com/experience/springtastic-celebration/?gclid=CjwKCAjwuYWSBhByEiwAKd_n_pV0gcymV9yBgun4KMCYv_j_wazNYyLL45I8L2eW6fmhooBzs9DVHxoCneQQAvD_BwE&gclsrc=aw.ds">
+            Our credentials
+          </a>
         </div>
         <div id="intro2">
           Just kidding lol
-          <br />
+          <br /><br />
           <button className="gatelock" onClick={() => this.introSwitch("2", "1")} >Yikes</button>
         </div>
       </div>

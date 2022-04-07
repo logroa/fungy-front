1abdea1d4347363cece39c1dc32532a5c2c46780
logroa <logroa@umich.edu>
2022-04-07 00:06:01 -0400

    write up

--- src/App.js
+++ src/App.js
@@ -41,21 +41,29 @@ class Intro extends React.Component {
            astute day trades, riverboat hold 'em coolers, and high profile government payoffs. But in reality for me, 
            it all boiled down to knowing the definition of the word “fungible”. Yep, that's it, baby.</p>
 
-          <p>What we're asking of you here is to join our community. Like any community, financial institution, or clothing
-            provider, our goal is to simply provide value.  We are selling t-shirts with a side of digital currency. Or vice 
-            versa if you prefer. Every shirt bought comes with its own unique NFT that will be made available to the owner
+          <p>
+            What we are doing is selling t-shirts with a side of digital currency. Or vice 
+            versa, whichever you prefer. Every shirt bought comes with its own unique NFT that will be made available to the owner
             of the corresponding shirt. Does this physical attachment defeat the purpose of the nonfungible token we're 
             providing? Probably, but we can't say for sure. That would require us actually understanding them and how they 
-            work, which, I cannot emphacize enough, we don't.</p>
+            work, which, I cannot emphasize enough, we don't.
+          </p>
 
-          <p>This opportunity comes to you free of risk.  All this site is doing presenting the opportunity to confirm interest 
-            politically efficacious, thought-provoking apparel, invest in cryptocurrency, or join a community full of former 
-            US Secretaries of Agriculture, martial arts champions, and degenerate sportsbettors free of charge. Payment will come
-            soon when shirts are printed and tokens are minted, if you are still even interested.  All profits will be put in your 
+          <p>
+            This opportunity comes to you free of risk.  All this site is doing presenting the opportunity to confirm interest 
+            politically efficacious, thought-provoking apparel, invest in cryptocurrency, and join a community full of former 
+            US Secretaries of Agriculture, martial arts champions, and degenerate sportsbettors free of charge. It's similar to 
+            LinkedIn but we connect you to people you might actually like! You can pay when shirts are printed and 
+            tokens are minted, if you are still even interested.
+          </p>
+            
+          <p>
+            All profits will be put in your 
             name towards a cryptocurrency hedgefund of sorts.  Is this an excuse to day-trade crypto with my buddies?  Absolutely. 
             But it's also about making friends and making you think I'm really smart. Sizable returns will be paid back to investors, 
             and on the off chance we do squander your money, you'll walk away with a shirt wearable in very few social settings!  You're 
-            welcome!  Again, my name is <b>Oscar</b>. Look me up!</p>
+            welcome!  Again, my name is <b>Oscar</b>. Look me up!
+          </p>
 
           <p>Click below to read more about how we got here and what we're hoping to do!</p>
           <br/>
